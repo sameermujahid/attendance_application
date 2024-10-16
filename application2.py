d17b271c1bd69e82d04883ee5aafea8b47751153
Shaik Sameer Mujahid <127492574+sameermujahid@users.noreply.github.com>
2024-10-16 11:10:48 +0530

    Update application2.py

--- application2.py
+++ application2.py
@@ -147,4 +147,4 @@ def export_attendance():
 
 # Run the Flask app with SocketIO
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
